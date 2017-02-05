0eb6c47095a63db0ef891be381851d011c2d0ea7
DavidPowell149 <david.powell.149@gmail.com>
2017-02-05 13:12:25 -0500

    Added some assets and initialized them

--- astroid-avoid/js/main.js
+++ astroid-avoid/js/main.js
@@ -14,42 +14,69 @@ window.onload = function()
 
     "use strict";
 
-    var game = new Phaser.Game( 600, 800, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 600, 760, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+
+    var earthHealth = 100;  // Earth's hitpoints
 
     function preload()
     {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        // Load in game assets
+        game.load.image( 'background', 'assets/space_background.png' );
+        game.load.image( 'earth', 'assets/earth.png' );
     }
 
     var bouncy;
 
     function create()
     {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+        // The space background
+        game.add.sprite(0, 0, "background");
+        var earth = game.add.sprite(-50, 580, "earth");     // Place it in bottom center
 
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
 
+
+
+
+        // // Create a sprite at the center of the screen using the 'logo' image.
+        // bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'earth' );
+        // // Anchor the sprite at its center, as opposed to its top-left corner.
+        // // so it will be truly centered.
+        // bouncy.anchor.setTo( 0.5, 0.5 );
+        //
+        // // Turn on the arcade physics engine for this sprite.
+        // game.physics.enable( bouncy, Phaser.Physics.ARCADE );
+        // // Make it bounce off of the world bounds.
+        // bouncy.body.collideWorldBounds = true;
+        //
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something terrible.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        var style = { font: "15px Verdana", fill: "#9999ff", align: "center" };
+        var text = game.add.text( 10, 5, "Health: " + earthHealth, style );
     }
 
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+    // Runs every tick/iteration/moment/second
+    function update()
+    {
+        updateHealth();     // Adjust the earth's health counter
+    }
+
+
+
+
+
+    function updateHealth()
+    {
+        if(earthHealth <= 0)
+        {
+            gameOver(); // The player has died
+        }
+
+        // var style = { font: "15px Verdana", fill: "#9999ff", align: "center" };
+        // var text = game.add.text( 10, 5, "Health: " + earthHealth, style );
+    }
+
+    function gameOver()
+    {
+        console.log("Game Over");
     }
 };
