97a1421f5eca566e20f82a2b887e430d2c663deb
DavidPowell149 <david.powell.149@gmail.com>
2017-02-14 13:45:52 -0500

    Initial commit for game 2
    Organ Race prototype

---
+++ organ-race/js/main.js
@@ -0,0 +1,42 @@
+window.onload = function()
+{
+    // You might want to start with a template that uses GameStates:
+    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
+
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+
+    "use strict";
+
+    // Global variables
+    var game = new Phaser.Game( 1100, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+
+
+    var music;
+
+    // Pre loads assets for game load
+    function preload()
+    {
+        // Load in game assets
+        //game.load.image( 'background', 'assets/space_background.png' );
+    }
+
+    // Called on game's initial creation state
+    function create()
+    {
+        // The space background
+        game.stage.backgroundColor = "FFFFCC";
+    }
+
+    // Runs every tick/iteration/moment/second
+    function update()
+    {
+
+    }
+};
